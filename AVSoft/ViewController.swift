cdc715d556c866dd3012277937f7a8da1e60c7cb
Roman Kovayin <kovajkin2012@yandex.ru>
2020-06-16 10:36:09 +0300

    Initial Commit

---
+++ AVSoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVSoft
+//
+//  Created by Роман Ковайкин on 16.06.2020.
+//  Copyright © 2020 Роман Ковайкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
